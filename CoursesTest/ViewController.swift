fec495daa59b6b7ed1eca22f89756b42b5db192d
you name <a@a.com>
2023-01-18 20:13:22 +0300

    Initial Commit

---
+++ CoursesTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CoursesTest
+//
+//  Created by ebtehal Sahli on 18/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+    }
+}
+
